309da281ee5be8230bad2133be5ebc57461045b7
Roman Kaniuka <roman.kaniuka.dev@gmail.com>
2026-03-06 16:02:01 +0200

    add bg

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,12 +2,14 @@ import { BrandBadge } from '@/components/ui/BrandBadge/BrandBadge';
 import s from './layout.module.scss';
 import { CrossIcon } from '@/components/icons/icons';
 import Link from 'next/link';
+import Image from 'next/image';
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
    return (
       <div className={s.container}>
          <Link href='/' className={s.cancelButton}>
             <CrossIcon />
          </Link>
+			<Image src='/BG.png' width={1420} height={832} alt='' className={s.backgroundImage} />
          <div className={s.card}>
             <div className={s.cardHeader}>
                <BrandBadge />
